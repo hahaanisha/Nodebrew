c6ee190695d393098ed7191b2fe6584c5637316f
Anisha Shankar <168823401+hahaanisha@users.noreply.github.com>
2025-07-12 22:43:08 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        {/* ✅ Google Tag Manager script */}
+{/*         {/* ✅ Google Tag Manager script */}
         <Script
           id="gtm-script"
           strategy="afterInteractive"
@@ -34,7 +34,7 @@ export default function RootLayout({
             `,
           }}
         />
-
+ */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
